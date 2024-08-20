1195332b6b8c3956d673a336e4f53f8c7ce6f694
naseemmsd <naseemmehsood932@gmail.com>
2024-08-20 22:32:21 +0500

    interfac

---
+++ interface/Program.cs
@@ -0,0 +1,37 @@
+﻿using @interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PartTimeEmployees pte = new PartTimeEmployees();
+            pte.show();
+            Console.ReadLine();
+        }
+    }
+    interface IEmployee
+    {
+        //int a {  get; set; }
+        void show();
+        
+       // int this [int index] { get; set; }
+       // event EventHandler envhndlr;
+
+    }
+    class PartTimeEmployees : IEmployee
+    {
+        public void show()
+        {
+            Console.WriteLine("This is a  Method of IEmployee Interface");
+        }
+
+        
+    }
+}
